90d8587c4e0dbecbc5840f5fb01a107f51554020
SonuPriyanshuok146 <priyanshuok146@yahoo.com>
2026-08-22 14:10:00 +0530

    Exception keyword use and use of try catch throw what and all

--- c++Placement/ExceptionHandling.cpp
+++ c++Placement/ExceptionHandling.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<exception>
 using namespace std;
 
+/*
 void fun(int a, int b){
     int c = a/b;
     cout << c;
@@ -10,5 +12,52 @@ int main(){
     int a, b;
     cin >> a >> b;
     fun(a, b);
+    return 0;
+}
+*/
+
+/*
+int main(){
+    try{
+        int age;
+        cin >> age;
+        if(age < 18){
+            throw age;
+        }
+        cout << "Eligible Age";
+    }
+    catch(int x){
+        cout << "Under age & age is : " << x;
+    }
+}
+*/
+
+/*
+int fun(int a, int b){
+    if(b == 0){
+        throw "Division by Zero";
+    }
+    return a/b;
+}
+
+int main(){
+    try{
+        int x, y;
+        cin >> x >> y;
+        int c = fun(x,y);
+    }
+    catch(const char *e){
+        cout <<e;
+    }
+}
+*/
+
+int main(){
+    try{
+        throw runtime_error("Division by Zero");
+    }
+    catch(exception &e){
+        cout << e.what();
+    }
     return 0;
 }
