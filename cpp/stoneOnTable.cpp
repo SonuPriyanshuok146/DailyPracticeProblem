a52f0a136c3295e49f7972fa517b8c132d4fbfd3
SonuPriyanshuok146 <priyanshuok146@yahoo.com>
2026-08-23 14:21:05 +0530

    Starting with java

---
+++ cpp/stoneOnTable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+
+    int ans = 0;
+    for(int i = 0; i < n-1; i++){
+        if(str[i] == str[i+1]){
+            ans++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
